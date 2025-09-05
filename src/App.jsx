d6229a212ac4718f0a5ac667854cace53059947d
Harsh2003-oss <harsh.sil872@gmail.com>
2025-09-05 12:28:26 +0530

    created firt button

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,10 @@ const App = () => {
 
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
 <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl '>
-
+<button
+className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+style={{backgroundColor:"red"}}
+>Red</button>
 </div>
       </div>
       
