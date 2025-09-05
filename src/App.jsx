fb129b8781c77356388e1a9b1b322e8050d9e082
Harsh2003-oss <harsh.sil872@gmail.com>
2025-09-05 12:35:13 +0530

    added events

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,22 @@ const App = () => {
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
 <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl '>
 <button
+onClick={() => setColor("red")}
 className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
 style={{backgroundColor:"red"}}
 >Red</button>
 
 <button
+onClick={() => setColor("green")}
 className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
 style={{backgroundColor:"green"}}
->Red</button>
+>Green</button>
 
 <button
+onClick={() => setColor("blue")}
 className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
 style={{backgroundColor:"blue"}}
->Red</button>
+>Blue</button>
 
 </div>
       </div>
